59e35dde82038b0d5e5edb2e419d108208b48b5b
mac <mac@Jade-MBP.local>
2019-01-09 11:22:58 +0700

    firebase hosting setup

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCFs20xWUFpWDwGojeDoZONuFyXzmqWvSM",
+    authDomain: "chanel-pwa.firebaseapp.com",
+    databaseURL: "https://chanel-pwa.firebaseio.com",
+    projectId: "chanel-pwa",
+    storageBucket: "chanel-pwa.appspot.com",
+    messagingSenderId: "1043672550483"
+  }
 };
