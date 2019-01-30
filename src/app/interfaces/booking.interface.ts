fb77fa3948a7d3dd9610125ff75c2026d441efd0
mac <mac@Jade-MBP.local>
2019-01-30 14:27:36 +0700

    change booking format

--- src/app/interfaces/booking.interface.ts
+++ src/app/interfaces/booking.interface.ts
@@ -1,17 +1,46 @@
 import { iUser } from '../interfaces/user.interface';
 import { iCustomer } from '../interfaces/customer.interface';
-import { iFacial} from '../interfaces/ifacial.interface';
+import { iFacialCabin } from './facialcabin.interface';
 
-export interface iBooking{
-    B_CUSTOMER: iCustomer,
-    B_FACIAL: iFacial,
-    B_FDATE: string,
-    B_FHOUR: string,
+export interface iBooking {
     B_ID: string,
+    B_CUSTOMER_NAME: string,
+    B_CUSTOMER_VIPCODE: string,
+    B_CUSTOMER_PHONE: string,
+    B_CUSTOMER_ID: string,
+    B_CUSTOMER: iCustomer,
+    B_FACIAL: iFacialCabin,
+    B_FACIAL_ID: string,
+    B_FACIAL_NAME: string,
     B_STAFF: iUser,
-    B_STATUS:string,
-    B_TDATE: string,
+    B_STAFF_ID: string,
+    B_STAFF_NAME: string,
+    B_SLOT: string,
+    B_DATE: string,
+    B_FHOUR: string,
     B_THOUR: string,
-    B_TIME:string,
-    B_NOTE: string
+    B_isNewCustomer: boolean,
+    B_CSCC: boolean,
+    B_SUBLIMAGE: boolean,
+    B_SUBLIMAGES: string[],
+    B_LELIFT: boolean,
+    B_FASHION: boolean,
+    B_FASHIONS: string[],
+    B_1stTIME: boolean,
+    B_NOTE: string,
+    B_STATUS: string,
+    B_CREATED_TIME: string,
+    B_TOTAL: number,
+    B_SPECIALIST: iUser,
+    B_SPECIALIST_ID: string,
+    B_SPECIALIST_NAME: string,
+    B_BA_BOOK: iUser,
+    B_BA_BOOK_ID: string,
+    B_BA_BOOK_NAME: string,
+    B_BA_SELL: iUser,
+    B_BA_SELL_ID: string,
+    B_BA_SELL_NAME: string,
+    B_SVC_BOOK: number,
+    B_SVC_USE: number,
+    B_SVC_CANCEL: number
 }
