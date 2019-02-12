65767a85b5786ecad33556d7bdb9cd8e51f4cd5c
mac <mac@Jade-MBP.local>
2019-02-12 15:06:01 +0700

    fix merge error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-
 import { StatusBar } from '@ionic-native/status-bar/ngx';
-<<<<<<< HEAD
-import { MessagingService } from "./shared/messaging.service";
-=======
 import { AuthService } from './services/auth.service';
->>>>>>> acb71b67687a4150839652607218c9da7c070edf
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
 })
-<<<<<<< HEAD
-export class AppComponent {
-  message;
-=======
 export class AppComponent implements OnInit {
->>>>>>> acb71b67687a4150839652607218c9da7c070edf
   public appPages = [
     { title: 'Home', url: '/home', icon: 'home' },
     { title: 'Customers', url: '/customers', icon: 'contacts' },
@@ -40,17 +31,9 @@ export class AppComponent implements OnInit {
     private platform: Platform,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar,
-<<<<<<< HEAD
-    private messagingService: MessagingService
-=======
     public authService: AuthService
->>>>>>> acb71b67687a4150839652607218c9da7c070edf
   ) {
     this.initializeApp();
-    const userId = 'WCZDdZHAKEbLjUbnXuseww33pzf1';
-    this.messagingService.requestPermission(userId)
-    this.messagingService.receiveMessage()
-    this.message = this.messagingService.currentMessage
   }
 
   initializeApp() {
