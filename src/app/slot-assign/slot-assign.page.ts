080a7523ac113c4a7f5a19b6ffec5dda63f0a8cc
mac <mac@Jade-MBP.local>
2019-02-21 11:32:52 +0700

    update/delete/add slot flexibly

--- src/app/slot-assign/slot-assign.page.ts
+++ src/app/slot-assign/slot-assign.page.ts
@@ -5,7 +5,7 @@ import { iUser } from '../interfaces/user.interface';
 import { iSlot } from '../interfaces/slot.interface';
 import { iDay } from '../interfaces/day.interface';
 import { AppService } from '../services/app.service';
-import { AlertController } from '@ionic/angular';
+import { AlertController, ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-slot-assign',
@@ -19,21 +19,24 @@ export class SlotAssignPage implements OnInit, OnDestroy {
   DaysInMonth: iDay[] = [];
   STATES = ['Available', 'Booked', 'Canceled', 'Completed', 'Expired'];
   TODAY: string;
-  COLOR_SPE:any = [];
-  
+  COLOR_SPE: any = [];
+
   Specialists = [];
   selectedSpecialist: iUser = null;
   NONE = {
     SPE_ID: '',
     SPE_NAME: ''
   }
+  selectedDay: iDay;
+  deleteUpdateSlotsMode = false;
   constructor(
     private alertCtrl: AlertController,
+    private actionSheetCtrl: ActionSheetController,
     private crudService: CrudService,
     private calendarService: CalendarService,
     private appService: AppService
-  ) { 
-    this.COLOR_SPE['']='Gray';
+  ) {
+    this.COLOR_SPE[''] = 'Gray';
   }
 
   ngOnInit() {
@@ -100,7 +103,7 @@ export class SlotAssignPage implements OnInit, OnDestroy {
       let USERS = [];
       qSnap.forEach(docSnap => {
         let USER = <iUser>docSnap.data();
-        this.COLOR_SPE[USER.U_ID]=this.getRandomColor();
+        this.COLOR_SPE[USER.U_ID] = this.getRandomColor();
         USERS.push(USER);
         this.Specialists = USERS.filter(U => U.U_ROLE == 'Specialist');
       })
@@ -111,6 +114,16 @@ export class SlotAssignPage implements OnInit, OnDestroy {
   }
 
   selectSlotInList(Day: iDay, SLOT: iSlot, i: number) {
+    if (this.deleteUpdateSlotsMode) {
+      console.log('update/delete slot in list')
+      this.alertActionDeleteOrUpdateslot(Day, SLOT, i);
+    } else {
+      this.updateSlotInList(Day, SLOT, i);
+    }
+
+  }
+
+  updateSlotInList(Day: iDay, SLOT: iSlot, i: number) {
     console.log(Day, SLOT, i);
     console.log(this.selectedSpecialist);
     if (this.selectedSpecialist) {
@@ -124,7 +137,6 @@ export class SlotAssignPage implements OnInit, OnDestroy {
     } else {
       this.appService.alertConfirmationShow(null, 'Select Specicalist please');
     }
-
   }
 
 
@@ -203,4 +215,181 @@ export class SlotAssignPage implements OnInit, OnDestroy {
     return color;
   }
 
+  updateSlots(Day: iDay) {
+    console.log(Day);
+    this.selectedDay = Day;
+    this.actionSheetOnUpdateSlots(Day);
+    // this.doAddNewSlot(Day);
+  }
+
+  async actionSheetOnUpdateSlots(Day: iDay) {
+    const actionSheet = await this.actionSheetCtrl.create({
+      header: null,
+      buttons: [{
+        text: 'Cập nhật / Xoá',
+        role: 'destructive',
+        icon: 'trash',
+        handler: () => {
+          console.log('Delete/Update clicked');
+          this.deleteUpdateSlotsMode = true;
+        }
+      }, {
+        text: 'New',
+        icon: 'add',
+        handler: () => {
+          this.doAddNewSlot(Day);
+        }
+      }, {
+        text: 'Cancel',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          console.log('Cancel clicked');
+          this.deleteUpdateSlotsMode = false;
+        }
+      }]
+    });
+    await actionSheet.present();
+  }
+
+  async doAddNewSlot(Day: iDay) {
+    const alert = await this.alertCtrl.create({
+      header: 'Chọn giờ',
+      inputs: [
+        {
+          name: 'slotStr',
+          type: 'text',
+          placeholder: 'Nhập giờ cho slot. Vd: 10:30'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Confirm Cancel');
+          }
+        }, {
+          text: 'OK',
+          handler: (data) => {
+            console.log('Confirm Ok', data);
+            this.addNewSlotIntoDay(Day, data.slotStr);
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
+  addNewSlotIntoDay(Day: iDay, TimeOfSlot: string) {
+    let SLOT: iSlot = {
+      BAB_ID: "",
+      BAB_NAME: "",
+      BAS_ID: "",
+      BAS_NAME: "",
+      BOOK_ID: "",
+      MAN_ID: "",
+      MAN_NAME: "",
+      SLOT: TimeOfSlot,
+      SPE_ID: "",
+      SPE_NAME: "",
+      STATUS: "AVAILABLE",
+    }
+    let _Slots = Day.Slots;
+    _Slots.push(SLOT);
+    _Slots.sort((a, b) => {
+      if (Number(a.SLOT.replace(':', '')) > Number(b.SLOT.replace(':', ''))) return 1;
+      if (Number(a.SLOT.replace(':', '')) < Number(b.SLOT.replace(':', ''))) return -1;
+      return 0;
+    })
+
+    Day.Slots = _Slots;
+    this.crudService.dayUpdate(Day);
+  }
+
+  async alertActionDeleteOrUpdateslot(Day: iDay, SLOT: iSlot, i: number) {
+    console.log(Day, SLOT, i);
+    const alert = await this.alertCtrl.create({
+      header: 'Cập nhật',
+      inputs: [
+        {
+          name: 'newSLotValue',
+          type: 'text',
+          value: SLOT.SLOT,
+          placeholder: 'Nhập thời gian slot mới'
+        },
+        {
+          disabled: true,
+          value: SLOT.SPE_NAME,
+          placeholder: 'Placeholder 2'
+        },
+        {
+          disabled: true,
+          value: SLOT.STATUS,
+          placeholder: 'Placeholder 2'
+        },
+        // {
+        //   name: 'name3',
+        //   value: 'http://ionicframework.com',
+        //   type: 'url',
+        //   placeholder: 'Favorite site ever'
+        // },
+        // // input date with min & max
+        // {
+        //   name: 'name4',
+        //   type: 'date',
+        //   min: '2017-03-01',
+        //   max: '2018-01-12'
+        // },
+        // // input date without min nor max
+        // {
+        //   name: 'name5',
+        //   type: 'date'
+        // },
+        // {
+        //   name: 'name6',
+        //   type: 'number',
+        //   min: -5,
+        //   max: 10
+        // },
+        // {
+        //   name: 'name7',
+        //   type: 'number'
+        // }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Confirm Cancel');
+            this.deleteUpdateSlotsMode = false;
+          }
+        }, {
+          text: 'Update',
+          handler: (data) => {
+            console.log('Confirm Ok', data);
+            SLOT.SLOT = data.newSLotValue;
+            Day.Slots[i] = SLOT;
+            this.crudService.dayUpdate(Day);
+            this.deleteUpdateSlotsMode = false;
+          }
+        }, {
+          text: 'Delete',
+          role: 'destructive',
+          handler: (data) => {
+            console.log('Confirm Ok', data);
+            Day.Slots.splice(i, 1);
+            this.crudService.dayUpdate(Day);
+            this.deleteUpdateSlotsMode = false;
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
 }
