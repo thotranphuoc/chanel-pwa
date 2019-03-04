be29f55e597151ea62f7841b2901e75193fdf93c
Luan Nguyen <luan@enablecode.vn>
2019-03-04 10:57:08 +0700

    L add change avatar for user

--- src/app/account/account.page.ts
+++ src/app/account/account.page.ts
@@ -4,9 +4,11 @@ import { AuthService } from '../services/auth.service';
 import { LocalService } from '../services/local.service';
 import { Subscription } from 'rxjs';
 import { CrudService } from '../services/crud.service';
-import { NavController } from '@ionic/angular';
+import { NavController, ModalController } from '@ionic/angular';
 import { AppService } from '../services/app.service';
-
+import { SetgetService } from '../services/setget.service';
+import { DbService } from '../services/db.service';
+import { UserPhotoTakePage } from '../user-photo-take/user-photo-take.page';
 @Component({
   selector: 'app-account',
   templateUrl: './account.page.html',
@@ -14,6 +16,8 @@ import { AppService } from '../services/app.service';
 })
 export class AccountPage implements OnInit, OnDestroy {
   USER: iUser;
+  base64Images: string[] = [];
+  hasNewAvatar: boolean = false;
   // isSigned = true;
   userSubcription: Subscription;
   constructor(
@@ -21,7 +25,10 @@ export class AccountPage implements OnInit, OnDestroy {
     public authService: AuthService,
     private localService: LocalService,
     private crudService: CrudService,
-    private appService: AppService
+    private appService: AppService,
+    private setGetService: SetgetService,
+    private modalCtrl: ModalController,
+    private dbService: DbService
   ) {
 
     // this.USER = this.localService.USER_DEFAULT;
@@ -78,6 +85,52 @@ export class AccountPage implements OnInit, OnDestroy {
       })
       .catch(err => {
         console.log(err);
+      })
+  }
+
+  async takePhoto() {
+    console.log('take Photo');
+    let photosModal = await this.modalCtrl.create({
+      component: UserPhotoTakePage,
+      componentProps: { PHOTOS: this.base64Images }
+    })
+    await photosModal.present();
+    const data: any = await photosModal.onDidDismiss();
+    console.log('show data');
+    console.log(data);
+    console.log('show image photo');
+    console.log(data.data.PHOTOS);
+    this.base64Images = data.data.PHOTOS;
+    this.hasNewAvatar = true;
+    console.log('show image');
+    console.log(this.base64Images);
+    this.uploadImageThenUpdateURL();
+  }
+
+  uploadImageThenUpdateURL() {
+    // console.log(this.PROFILE);
+    this.dbService.uploadBase64Image2FBReturnPromiseWithURL('USERS/' + this.USER.U_ID, this.base64Images[0], this.USER.U_ID)
+      .then((downloadURL: string) => {
+        console.log(downloadURL);
+        this.USER.U_AVATAR = downloadURL;
+        console.log(this.USER);
+        this.updateUser();
+        // this.onUpdateProfile();
+      })
+      .catch((err) => console.log(err));
+  }
+
+  updateUser() {
+    console.log(this.USER);
+    this.crudService.userUpdate(this.USER)
+      .then((res) => {
+        console.log(res);
+        this.appService.alertShow('Thành công', null, 'Cập nhật thành công');
+        this.navCtrl.goBack();
+      })
+      .catch(err => {
+        console.log(err);
+        this.appService.alertShow('Thất bại', null, 'Cập nhật thất bại');
       })
   }
 
